3d4a18a63023c0491414c795a25a58e3066ac076
Anurag Singh <anuragsingh.vs98@icloud.com>
2024-09-24 17:09:41 +0530

    mongoose connection done

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    Name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    }
+});
+
+const User = new mongoose.model('User' , userSchema);
+
+module.exports = User
--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
-const express = require('express')
-const app = express()
-PORT = 7777
+const express = require('express');
+const app = express();
+const connectDB = require('../constants/db_connection')
+const User = require('../models/user')
+PORT = 7777;
 
-app.get('/testing', (req,res)=>{
-    res.send('Hello World')
+connectDB();
+
+app.post('/user' , async (req,res)=>{
+    const user = new User({
+        Name: "Anurag",
+        age: 21,
+    })
+    await user.save()
+
+    res.send("User sent")
 })
 
-app.listen(PORT)
+app.listen(PORT);
